c6741c8cf5c65dc25594bab6ca5c7f388229a184
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-01-08 18:50:40 +0100

    gdal recipe added

---
+++ gdal/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./configure LIBS="-ludunits2" --with-python --prefix=$PREFIX \
+--with-geos=$PREFIX/bin/geos-config \
+--with-static-proj4=$PREFIX \
+--with-hdf5=$PREFIX \
+--with-netcdf=$PREFIX \
+--with-xerces=$PREFIX \
+--with-jpeg=$PREFIX \
+--with-jasper=$PREFIX \
+--without-pam \
+--disable-rpath
+make
+make install
+
+# Copy data files 
+mkdir -p $PREFIX/share/gdal/
+cp data/*csv $PREFIX/share/gdal/
+cp data/*wkt $PREFIX/share/gdal/
+
