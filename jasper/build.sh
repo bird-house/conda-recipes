ec38e9446d47dee0d95038947e701994b21dd4e9
Carsten Ehbrecht <carsten@linacs.org>
2015-04-29 22:50:03 +0200

    fixed jasper on macosx

--- jasper/build.sh
+++ jasper/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LDFLAGS=-L$LD_RUN_PATH
+#export LDFLAGS=-L$LD_RUN_PATH
 export CFLAGS="-I$PREFIX/include"
 sh configure --prefix=$PREFIX --enable-shared --disable-debug --disable-dependency-tracking
 
