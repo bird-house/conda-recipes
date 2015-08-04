7c99cfdbcb8ac8f7c4e729515a471e1aef65dea7
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-04 13:30:19 +0200

    java-jdk: fixed jre libs post link

--- java-jdk/post-link.sh
+++ java-jdk/post-link.sh
@@ -2,3 +2,5 @@
 
 ln -s $PREFIX/jre/lib/amd64/libjava.so $PREFIX/lib/libjava.so
 ln -s $PREFIX/jre/lib/amd64/server/libjvm.so $PREFIX/lib/libjvm.so
+ln -s $PREFIX/jre/lib/amd64/libnet.so $PREFIX/lib/libnet.so
+ln -s $PREFIX/jre/lib/amd64/libverify.so $PREFIX/lib/libverify.so
