103686d81647a7786a0213a68daf48227a58282a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-04 16:35:47 +0200

    update cdat-lite 6.0rc2

--- cdat-lite/build.sh
+++ cdat-lite/build.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
+#
 
-# CDAT-lite v5.2 finds the HDF5 library by checking for the static version,
-# so we pretend it exists.
-touch $PREFIX/lib/libhdf5.a
-
-# cdat_lite-5.2-1.tar.gz ships with broken symlinks which get in the way
-# of setup.py getting started, so we remove them.
-rm lib/MV2.py
-rm lib/cdat_info.py
-
+# Make sure the build can find NetCDF4 and HDF5:
 export NETCDF_HOME=$PREFIX
 export HDF5_HOME=$PREFIX
-$PYTHON setup.py install --prefix=$PREFIX
 
-# Tidy up the fake static library.
-rm $PREFIX/lib/libhdf5.a
+# Build the library:
+python setup.py install
