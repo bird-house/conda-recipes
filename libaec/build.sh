2a53adad394080a442ce134cbf37e7ec678fa29f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-05 14:43:25 +0200

    libaec added

---
+++ libaec/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./configure --prefix=$PREFIX
+make install
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
