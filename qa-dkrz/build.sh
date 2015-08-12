1d8b4398121cc1499ad8b0a647cb1487b6403724
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-12 10:51:56 +0200

    working on qa-dkrz

--- qa-dkrz/build.sh
+++ qa-dkrz/build.sh
@@ -1,4 +1,4 @@
-#/bin/bash -eu
+#!/bin/bash -eu
 
 echo "CC=\"/usr/bin/gcc\"" > install_configure
 echo "CXX=\"/usr/bin/g++\"" >> install_configure
@@ -6,8 +6,15 @@ echo "CFLAGS=\"-O2 -Wall\"" >> install_configure
 echo "CXXFLAGS=\"-O2 -Wall -D NC4\"" >> install_configure
 echo "LIB=\"$PREFIX/lib\"" >> install_configure
 echo "INCLUDE=\"$PREFIX/include\"" >> install_configure
-#echo "BIN=\"$PREFIX/bin\"" >> install_configure
 
-./install --prefix=$PREFIX CF
+export QA_PATH="$PWD"
+./install CF
+
+cp -r ./bin $PREFIX
+cp ./scripts/dkrz-cf-checker $PREFIX/bin
+cp -r ./tables $PREFIX
+
+
+
 
 
