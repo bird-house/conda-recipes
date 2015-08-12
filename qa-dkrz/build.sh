a71a2bef0a59463a50f60011ea9eb8542338493f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-12 15:15:55 +0200

    added lib/include for uuid

--- qa-dkrz/build.sh
+++ qa-dkrz/build.sh
@@ -4,8 +4,8 @@ echo "CC=\"/usr/bin/gcc\"" > install_configure
 echo "CXX=\"/usr/bin/g++\"" >> install_configure
 echo "CFLAGS=\"-O2 -Wall\"" >> install_configure 
 echo "CXXFLAGS=\"-O2 -Wall -D NC4\"" >> install_configure
-echo "LIB=\"${PREFIX}/lib\"" >> install_configure
-echo "INCLUDE=\"${PREFIX}/include\"" >> install_configure
+echo "LIB=\"${PREFIX}/lib\":/lib64" >> install_configure
+echo "INCLUDE=\"${PREFIX}/include\":/usr/include/uuid" >> install_configure
 
 # run build
 export QA_PATH="$PWD"
