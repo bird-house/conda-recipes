fc8295ecabcdc5d409d4f4a52141bda725f8150d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-14 14:40:57 +0200

    wicken and cf_units from ioos added

---
+++ cf_units/bld.bat
@@ -0,0 +1,6 @@
+set SITECFG=cf_units/etc/site.cfg
+echo [System] > %SITECFG%
+echo udunits2_path = %SCRIPTS%\udunits2.dll >> %SITECFG%
+
+
+"%PYTHON%" setup.py install --single-version-externally-managed  --record record.txt
