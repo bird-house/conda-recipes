22123072bbe96f7a0dc40c60ae7dee05de68e109
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-11-10 13:38:26 +0100

    update grib-api 1.14.2

--- grib_api/build.sh
+++ grib_api/build.sh
@@ -5,9 +5,7 @@ export CFLAGS="-O2 -fPIC"
 ./configure --prefix=$PREFIX \
     --enable-shared \
     --enable-python \
-    --disable-fortran \
     --with-jasper=$PREFIX
-#    --with-netcdf=$PREFIX
 make
 make install
 
