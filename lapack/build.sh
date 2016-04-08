31f679327535d4420e36c5e471eab985dffefcf0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-04-08 17:00:47 +0200

    update lapack

--- lapack/build.sh
+++ lapack/build.sh
@@ -3,10 +3,11 @@ cd build
 
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_Fortran_COMPILER=gfortran \
   -DBUILD_TESTING=OFF \
   -DBUILD_SHARED_LIBS=OFF \
   -DLAPACKE=ON \
   ..
 
-make
+make -j${CPU_COUNT}
 make install
