4d138899fef39a41a8d94b62f295fcf63571e6a8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-04-08 16:03:26 +0200

    fixed lapack95 build

--- lapack95/build.sh
+++ lapack95/build.sh
@@ -1,4 +1,7 @@
 cp $RECIPE_DIR/make.inc .
  
 cd SRC/
-make PREFIX=$PREFIX -j${CPU_COUNT} single_double_complex_dcomplex
+make PREFIX=$PREFIX single_double_complex_dcomplex
+
+cp ../lapack95.a $PREFIX/lib
+cp ../lapack95_modules/*.mod $PREFIX/include
