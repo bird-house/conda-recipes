0387427557054a7c712c25b9c585a16fb8954a36
Mac Pingu <ehbrecht@dkrz.de>
2016-04-09 16:47:51 +0200

    update castf90

--- castf90/build.sh
+++ castf90/build.sh
@@ -1,11 +1,1 @@
-# use our own conda makefile
-cp $RECIPE_DIR/Makefile .
-
-make
-
-cp analogue.out $PREFIX/bin/analogue.out
-
-castf90_home="$PREFIX/opt/castf90"
-mkdir -p $castf90_home
-
-cp *.sh *.pdf pbsscript $castf90_home
+make -f Makefile.conda install
