7c9f939b149b6fca28f7a87562cf76b0a30345cb
Mac Pingu <ehbrecht@dkrz.de>
2016-04-09 14:23:07 +0200

    build static and shared libs for lapack

--- lapack/build.sh
+++ lapack/build.sh
@@ -1,10 +1,25 @@
 mkdir build
 cd build
 
+# build static libs
+cmake \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DBUILD_TESTING=ON \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DUSE_OPTIMIZED_BLAS=OFF \
+  -DLAPACKE=ON \
+  -DBUILD_DEPRECATED=ON \
+  ..
+ 
+make -j${CPU_COUNT}
+make install
+
+# build shared libs
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DBUILD_TESTING=ON \
   -DBUILD_SHARED_LIBS=ON \
+  -DUSE_OPTIMIZED_BLAS=OFF \
   -DLAPACKE=ON \
   -DBUILD_DEPRECATED=ON \
   ..
