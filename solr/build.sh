01a9d5d16280e063311e933d32a6a6a14ee73f21
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-25 19:54:44 +0100

    update solr 5.5.3

--- solr/build.sh
+++ solr/build.sh
@@ -4,7 +4,8 @@ if [ "$(uname)" == "Darwin" ]; then
     export JAVA_HOME=$(/usr/libexec/java_home)
     #export JRE_HOME=${JAVA_HOME}/jre
 else
-    export JAVA_HOME="/usr/lib/jvm/default-java"
+    export JAVA_HOME="${PREFIX}"
+    #export JAVA_HOME="/usr/lib/jvm/default-java"
     #export JRE_HOME="/usr/lib/jvm/java-7-openjdk-amd64/jre"
 fi
 
